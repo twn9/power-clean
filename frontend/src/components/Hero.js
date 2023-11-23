361f80edc86b6b9903d3fd96e958e39e7377a658
Tim Na <timothyna@naver.com>
2023-11-23 18:35:04 -0500

    updated hero image with extra options

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,12 +1,20 @@
+import washer from '../images/washer.png';
+import hero2 from '../images/hero2.png';
+
 export default function Hero() {
-    return (
-      <div className="bg-blue-100 min-h-screen flex items-center justify-center">
-        <div className="container mx-auto px-4 py-10 text-center">
-          <h2 className="text-3xl font-bold text-blue-700">Professional Power Washing</h2>
-          <p className="text-gray-600 mt-2">Making your driveway look new!</p>
+  return (
+    <div className="min-h-screen flex items-center justify-center" style={{ backgroundImage: `url(${hero2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="container mx-auto px-4 py-10 flex items-center justify-between">
+        {/* Text Content */}
+        <div className="text-left">
+          <h2 className="text-3xl font-bold text-white">Professional Power Washing</h2>
+          <p className="text-gray-200 mt-2">Making your driveway look new!</p>
           <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Request a quote</button>
         </div>
+        
+        {/* Washer Image */}
+        {/* <img src={washer} alt="washer" className="h-auto w-64" /> */}
       </div>
-    );
-  };
-  
+    </div>
+  );
+}

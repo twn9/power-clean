8365b461d8642b16c3691daf69901778f6ba2900
Tim Na <timothyna@naver.com>
2023-11-23 23:39:26 -0500

    imporved read me

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # power-clean
+
+This website uses [FastAPI](https://fastapi.tiangolo.com/) for the backend and [React](https://reactjs.org/)for the frontend.
+
+## Running the project
+
+### Backend
+1. Go to the `backend` directory
+2. Install the dependencies by running `pip install -r requirements.txt`
+3. Start the server by running `python main.py`
+
+```source venv/bin/activate``` to activate venv
+
+The backend will be running on `http://localhost:8000/`
+
+### Frontend
+1. Go to the `frontend` directory
+2. Install the dependencies by running `npm install`
+3. Start the development server by running `npm start`
+
+The frontend will be running on `http://localhost:3000/`
--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,14 +1,15 @@
 import washer from '../images/washer.png';
 import hero2 from '../images/hero2.png';
+import hero3 from '../images/hero3.png'
 
 export default function Hero() {
   return (
-    <div className="min-h-screen flex items-center justify-center" style={{ backgroundImage: `url(${hero2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="min-h-screen flex items-center justify-center" style={{ backgroundImage: `url(${hero3})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="container mx-auto px-4 py-10 flex items-center justify-between">
         {/* Text Content */}
         <div className="text-left">
           <h2 className="text-3xl font-bold text-white">Professional Power Washing</h2>
-          <p className="text-gray-200 mt-2">Making your driveway look new!</p>
+          <p className="text-gray-200 mt-2">Making your house look new!</p>
           <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Request a quote</button>
         </div>
         
